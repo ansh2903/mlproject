b005a64afcd9f686d13dbf6778671ce2d67b845a
Ansh Sharma <anshsharma2903@gmail.com>
2025-05-14 23:53:44 +0530

    EDA and Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() #_ is the first argument, __ is the second argument, and exc_tb is the third argument and it is the traceback object which provides on while file the exception has occured and on which line
@@ -12,7 +12,7 @@ def error_message_detail(error,error_detail:sys):
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail = error_detail)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
